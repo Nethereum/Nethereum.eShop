eec1190faa2983f2f26d47804e9b25f6688238fc
KevinSmall <kevin_small@hotmail.com>
2020-01-22 14:24:24 +0000

    Revised business partner storage
    upgrade to sol 0.6.1
    removed b2b fields
    added oneclickdapp links

--- src/Nethereum.Commerce.Contracts/Contracts/BusinessPartnerStorage.sol
+++ src/Nethereum.Commerce.Contracts/Contracts/BusinessPartnerStorage.sol
@@ -1,90 +1,58 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.6.1;
 pragma experimental ABIEncoderV2;
 
 import "./IBusinessPartnerStorage.sol";
 import "./IEternalStorage.sol";
 import "./IAddressRegistry.sol";
-import "./StringLib.sol";
+import "./StringConvertible.sol";
 
-contract BusinessPartnerStorage is IBusinessPartnerStorage
+contract BusinessPartnerStorage is IBusinessPartnerStorage, StringConvertible
 {
     IEternalStorage public eternalStorage;
     IAddressRegistry public addressRegistry;
 
-    string constant private MAP_SELLER_GET_BUYER_VIEW_VENDOR = "mapSellerGetBuyerViewVendor";
-    string constant private MAP_BUYER_GET_SELLER_VIEW_CUSTOMER = "mapBuyerGetSellerViewCustomer";
     string constant private MAP_SYSTEM_ID_TO_WALLET_ADDRESS = "mapSystemIdToWalletAddress";
     string constant private MAP_SYSTEM_ID_TO_DESCRIPTION = "mapSystemIdToDescription";
 
-    constructor (address contractAddressOfRegistry) public payable
+    constructor (address contractAddressOfRegistry) public
     {
         addressRegistry = IAddressRegistry(contractAddressOfRegistry);
     }
 
     /// Configure contract
     /// @param nameOfEternalStorage key of the entry in the address registry that holds the eternal storage contract address
-    function configure(string memory nameOfEternalStorage) public
+    function configure(string memory nameOfEternalStorage) override public
     {
         // Eternal storage
         eternalStorage = IEternalStorage(addressRegistry.getAddressString(nameOfEternalStorage));
         require(address(eternalStorage) != address(0), "Could not find EternalStorage address in registry");
     }
 
-    /// @dev Get mapping from [buyer system id + seller system id] to get [customer id in seller system that represents the buyer system id]
-    function getSellerViewCustomerIdForBuyerSysId(bytes32 buyerSystemId, bytes32 sellerSystemId) external view returns (bytes32 sellerViewCustomerId)
-    {
-        bytes32 mappingKey = keccak256(abi.encodePacked(buyerSystemId, sellerSystemId));
-        return eternalStorage.getMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_BUYER_GET_SELLER_VIEW_CUSTOMER), mappingKey);
-    }
-
-    /// @dev Store mapping with key [buyer system id + seller system id] to store [customer id in seller system that represents the buyer system id]
-    function setSellerViewCustomerIdForBuyerSysId(bytes32 buyerSystemId, bytes32 sellerSystemId, bytes32 sellerViewCustomerId) external
-    {
-        bytes32 mappingKey = keccak256(abi.encodePacked(buyerSystemId, sellerSystemId));
-        eternalStorage.setMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_BUYER_GET_SELLER_VIEW_CUSTOMER), mappingKey, sellerViewCustomerId);
-    }
-
-    /// @dev Get mapping from [buyer system id + seller system id] to get [vendor id in buyer system that represents the seller system id]
-    /// @dev Only needed if buyer system is an ERP-like system itself and needs a vendor number for its PO
-    function getBuyerViewVendorIdForSellerSysId(bytes32 buyerSystemId, bytes32 sellerSystemId) external view returns (bytes32 buyerViewVendorId)
-    {
-        bytes32 mappingKey = keccak256(abi.encodePacked(buyerSystemId, sellerSystemId));
-        return eternalStorage.getMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_SELLER_GET_BUYER_VIEW_VENDOR), mappingKey);
-    }
-
-    /// @dev Store mapping with key [buyer system id + seller system id] to store [vendor id in buyer system that represents the seller system id]
-    /// @dev Only needed if buyer system is an ERP-like system itself and needs a vendor number for its PO
-    function setBuyerViewVendorIdForSellerSysId(bytes32 buyerSystemId, bytes32 sellerSystemId, bytes32 buyerViewVendorId) external
-    {
-        bytes32 mappingKey = keccak256(abi.encodePacked(buyerSystemId, sellerSystemId));
-        eternalStorage.setMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_SELLER_GET_BUYER_VIEW_VENDOR), mappingKey, buyerViewVendorId);
-    }
-
     /// @dev Get mapping from [system id] to get [wallet address for that system id]
-    function getWalletAddress(bytes32 systemId) external view returns (address walletAddress)
+    function getWalletAddress(bytes32 systemId) override external view returns (address walletAddress)
     {
         bytes32 mappingKey = keccak256(abi.encodePacked(systemId));
-        return eternalStorage.getMappingBytes32ToAddressValue(StringLib.stringToBytes32(MAP_SYSTEM_ID_TO_WALLET_ADDRESS), mappingKey);
+        return eternalStorage.getMappingBytes32ToAddressValue(stringToBytes32(MAP_SYSTEM_ID_TO_WALLET_ADDRESS), mappingKey);
     }
 
     /// @dev Store mapping with key [system id] to store [wallet address for that system id]
-    function setWalletAddress(bytes32 systemId, address walletAddress) external
+    function setWalletAddress(bytes32 systemId, address walletAddress) override external
     {
         bytes32 mappingKey = keccak256(abi.encodePacked(systemId));
-        eternalStorage.setMappingBytes32ToAddressValue(StringLib.stringToBytes32(MAP_SYSTEM_ID_TO_WALLET_ADDRESS), mappingKey, walletAddress);
+        eternalStorage.setMappingBytes32ToAddressValue(stringToBytes32(MAP_SYSTEM_ID_TO_WALLET_ADDRESS), mappingKey, walletAddress);
     }
 
     /// @dev Get mapping from [system id] to get [description for that system id]
-    function getSystemDescription(bytes32 systemId) external view returns (bytes32 systemDescription)
+    function getSystemDescription(bytes32 systemId) override external view returns (bytes32 systemDescription)
     {
         bytes32 mappingKey = keccak256(abi.encodePacked(systemId));
-        return eternalStorage.getMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_SYSTEM_ID_TO_DESCRIPTION), mappingKey);
+        return eternalStorage.getMappingBytes32ToBytes32Value(stringToBytes32(MAP_SYSTEM_ID_TO_DESCRIPTION), mappingKey);
     }
 
     /// @dev Store mapping with key [system id] to store [description for that system id]
-    function setSystemDescription(bytes32 systemId, bytes32 systemDescription) external
+    function setSystemDescription(bytes32 systemId, bytes32 systemDescription) override external
     {
         bytes32 mappingKey = keccak256(abi.encodePacked(systemId));
-        eternalStorage.setMappingBytes32ToBytes32Value(StringLib.stringToBytes32(MAP_SYSTEM_ID_TO_DESCRIPTION), mappingKey, systemDescription);
+        eternalStorage.setMappingBytes32ToBytes32Value(stringToBytes32(MAP_SYSTEM_ID_TO_DESCRIPTION), mappingKey, systemDescription);
     }
 }
--- src/Nethereum.Commerce.Contracts/README.md
+++ src/Nethereum.Commerce.Contracts/README.md
@@ -18,4 +18,5 @@ Contract owner: 0x32A555F2328e85E489f9a5f03669DC820CE7EBe9
 | Contract Name | Address | Link to dApp | 
 |--|--|--|
 | Address Registry | 0x2d6efd93953ed8d5a8b2c6c01b2a603b4dc844e5 | https://oneclickdapp.com/giant-single/ |
-
+| Eternal Storage | 0xa56b266d4fa10d3c1feb95520aeed5a845b7ee89 | https://oneclickdapp.com/podium-mustang/ |
+| Business Partner Storage | 0xa0593ffc3cce2e330ab4d5996f5ffd9e50345f9f | https://oneclickdapp.com/virus-level/ |
