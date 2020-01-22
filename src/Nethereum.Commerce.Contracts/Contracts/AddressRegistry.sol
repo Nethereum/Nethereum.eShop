a656f367b353d92c5103bafb58b30d39d119a8d9
KevinSmall <kevin_small@hotmail.com>
2020-01-22 11:19:31 +0000

    Revised address registry, ownable

--- src/Nethereum.Commerce.Contracts/Contracts/AddressRegistry.sol
+++ src/Nethereum.Commerce.Contracts/Contracts/AddressRegistry.sol
@@ -1,16 +1,15 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.6.1;
 pragma experimental ABIEncoderV2; 
 
 import "./IAddressRegistry.sol";
 import "./Ownable.sol";
-import "./StringLib.sol";
+import "./StringConvertible.sol";
 
 /// @title Address Registry
 /// @notice Central repository of all contract addresses
 /// @notice Store is key value pair of free text identifier, address eg: "Dai", 0x12890D2cce102216644c59daE5baed380d84830c.
 /// @notice Address value of 0x0 means key not used yet. Address value of 0x1 means key was used then de-registered.
-/// @dev //TODO Ideally would like ANYONE to be able to add new, but only owner to change existing 
-contract AddressRegistry is IAddressRegistry, Ownable
+contract AddressRegistry is IAddressRegistry, Ownable, StringConvertible
 {
     event ContractAddressRegistered(bytes32 indexed contractName, address indexed contractAddress);
     event ContractAddressChanged(bytes32 indexed contractName, address indexed oldContractAddress, address indexed newContractAddress);
@@ -32,9 +31,9 @@ contract AddressRegistry is IAddressRegistry, Ownable
     
     /// @dev Main entry point for address registration
     /// @param a the address to register must be > 0x0. Set address to 0x1 to de-register an address
-    function registerAddress(bytes32 contractName, address a) public //onlyOwner()
+    function registerAddress(bytes32 contractName, address a) override public onlyOwner()
     {
-        require(a != address(0), "Address must be > 0x0, use 0x1 to de-register an address");
+        require(a != address(0), "Address cannot be 0x0, use 0x1 to de-register an address");
         
         address existingAddress = addressMap[contractName];
         if (existingAddress == address(0))
@@ -53,46 +52,32 @@ contract AddressRegistry is IAddressRegistry, Ownable
         }
     }
     
-    // @dev Helper fn to allow passing strings which will be truncated to 32
-    function registerAddressString(string memory contractName, address a) public //onlyOwner()
+    function registerAddressString(string memory contractName, address a) override public onlyOwner()
     {
-        bytes32 contractNameAsBytes32 = StringLib.stringToBytes32(contractName);
+        bytes32 contractNameAsBytes32 = stringToBytes32(contractName);
         registerAddress(contractNameAsBytes32, a);
     }
     
-    function getAddress(bytes32 contractName) public view returns (address a)
+    function getAddress(bytes32 contractName) override public view returns (address a)
     {
         return addressMap[contractName];
     }
     
-    function getAllAddresses() public view returns (string[] memory contractNames, address[] memory contractAddresses)
+    function getAllAddresses() override public view returns (string[] memory contractNames, address[] memory contractAddresses)
     {
         contractNames = new string[](addressCount);
         contractAddresses = new address[](addressCount);
         for (uint i = 0; i < addressCount; i++)
         {
-            contractNames[i] = StringLib.bytes32ToString(addressList[i], 0);
+            contractNames[i] = bytes32ToString(addressList[i], 0);
             contractAddresses[i] = addressMap[addressList[i]];
         }
     }
-    
-    /* nethereum code gen doesn't like below
-    function getAllAddressesPaired() public view returns (addressPair[] memory addressPairs)
-    {
-        addressPairs = new addressPair[](addressCount);
-        for (uint i = 0; i < addressCount; i++)
-        {
-            addressPair memory ap;
-            ap.contractName = StringLib.bytes32ToString(addressList[i], 0); 
-            ap.a = addressMap[addressList[i]];
-            addressPairs[i] = ap;
-        }
-    }*/
-    
+
     // @dev helper fn to allow passing strings which will be truncated to 32
-    function getAddressString(string memory contractName) public view returns (address a)
+    function getAddressString(string memory contractName) override public view returns (address a)
     {
-        bytes32 contractNameAsBytes32 = StringLib.stringToBytes32(contractName);
+        bytes32 contractNameAsBytes32 = stringToBytes32(contractName);
         return getAddress(contractNameAsBytes32);
     }
 }
--- src/Nethereum.Commerce.Contracts/README.md
+++ src/Nethereum.Commerce.Contracts/README.md
@@ -9,3 +9,13 @@ Smart Contract Solidity source code and the generated C# APIs to interact with t
 |--|--| 
 | `Contracts` | `.sol` source files and the `nethereum-gen.settings` file used to control API generation.
 | _contractname_ | Generated API for _contractname_. |
+
+
+### Contract Deployments on Rinkeby
+
+Contract owner: 0x32A555F2328e85E489f9a5f03669DC820CE7EBe9
+
+| Contract Name | Address | Link to dApp | 
+|--|--|--|
+| Address Registry | 0x2d6efd93953ed8d5a8b2c6c01b2a603b4dc844e5 | https://oneclickdapp.com/giant-single/ |
+
