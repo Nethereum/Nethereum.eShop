5cd266b284abd29f8de29432c76e6fcd151e279d
KevinSmall <kevin_small@hotmail.com>
2020-01-29 17:17:03 +0000

    Revising app layer contracts - wallets (WIP)

--- src/contracts/Nethereum.Commerce.Contracts/Contracts/IWalletBuyer.sol
+++ src/contracts/Nethereum.Commerce.Contracts/Contracts/IWalletBuyer.sol
@@ -1,13 +1,31 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.6.1;
 pragma experimental ABIEncoderV2;
 
 import "./IPoTypes.sol";
 
 interface IWalletBuyer
 {
+    // ##Revised
+    event PurchaseCreateRequestReceivedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    event PurchaseCreatedOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    event PurchaseCreateRequestFailedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    
+    // Buyer Wallet <= PoMain (event pass through)
+    event PurchaseItemApprovedOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    event PurchaseItemRejectedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+  
+    
+    
+    
+    // ##For reference
+    event PurchasePaymentMadeOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint64 indexed ethPurchaseOrderNumber, IPoTypes.Po po);
+    event PurchasePaymentFailedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint64 indexed ethPurchaseOrderNumber, IPoTypes.Po po);
+    event PurchaseRefundMadeOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint64 indexed ethPurchaseOrderNumber, IPoTypes.Po po);
+    event PurchaseRefundFailedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint64 indexed ethPurchaseOrderNumber, IPoTypes.Po po);
+    
     // Buyer Wallet => PoMain
-    function createPurchaseOrder(IPoTypes.Po calldata po) external;
-    function cancelPurchaseOrder(uint64 ethPoNumber) external;
+    function requestPoCreate(IPoTypes.Po calldata po) external;
+    function requestPoItemCancel(uint poNumber, uint8 poItemNumber) external;
     
     // Buyer Wallet <= PoMain (event pass through)
     function onPurchaseUpdatedWithSalesOrder(IPoTypes.Po calldata po) external;
@@ -15,7 +33,4 @@ interface IWalletBuyer
     function onPurchasePaymentFailed(IPoTypes.Po calldata po) external;
     function onPurchaseRefundMadeOk(IPoTypes.Po calldata po) external;
     function onPurchaseRefundFailed(IPoTypes.Po calldata po) external;
-    function onSalesOrderNotApproved(IPoTypes.Po calldata po) external;
-    function onSalesOrderCancelFailure(IPoTypes.Po calldata po) external;
-    function onSalesOrderInvoiceFault(IPoTypes.Po calldata po) external;
 }
--- src/contracts/Nethereum.Commerce.Contracts/Contracts/IWalletSeller.sol
+++ src/contracts/Nethereum.Commerce.Contracts/Contracts/IWalletSeller.sol
@@ -1,19 +1,30 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.6.1;
 pragma experimental ABIEncoderV2;
 
 import "./IPoTypes.sol";
 
 interface IWalletSeller
 {
+    // ##Revised
+    event PurchaseCreatedOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    event PurchaseCreateRequestFailedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    
+    event PurchaseItemApprovedOkLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+    event PurchaseItemRejectedLog(bytes32 indexed buyerAddress, bytes32 indexed sellerSysId, uint indexed poNumber, IPoTypes.Po po);
+  
+    // PoMain <= Seller Wallet
+    function setPoItemAddSalesOrderItem(uint poNumber, uint8 poItemNumber, bytes32 soNumber, bytes32 soItemNumber) external;
+    function setPoItemRejected(uint poNumber, uint8 poItemNumber) external;
+    
+    
+    
+    
+    // ##For reference
     // PoMain => Seller Wallet (event pass through)
     function onCreatePurchaseOrderRequested(IPoTypes.Po calldata po) external;
-    function onCancelPurchaseOrderRequested(IPoTypes.Po calldata po) external;
+    function onCancelPurchaseOrderItemRequested(uint poNumber, uint8 poItemNumber) external;
     
-    // PoMain <= Seller Wallet
-    function setSalesOrderNumberByEthPoNumber(uint64 ethPoNumber, bytes32 sellerSalesOrderNumber) external;
-    function refundPoToBuyer(uint64 ethPoNumber) external;
-    function releasePoFundsToSeller(uint64 ethPoNumber) external;
-    function reportSalesOrderNotApproved(uint64 ethPoNumber) external;
-    function reportSalesOrderCancelFailure(uint64 ethPoNumber) external;
-    function reportSalesOrderInvoiceFault(uint64 ethPoNumber) external;
+    function setPoItemGoodsIssued(uint poNumber, uint8 poItemNumber) external;
+    function requestPoItemEscrowRelease(uint poNumber, uint8 poItemNumber) external;
+    
 }
