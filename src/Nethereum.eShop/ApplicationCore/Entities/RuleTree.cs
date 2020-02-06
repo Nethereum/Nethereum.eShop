2b5172aa40dbda018be72357c2c774c06e4a719d
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-02-06 18:13:10 -0500

    Added initial batch of classes that aim to build a service for initializing and executing RuleTrees within the Wonka rules engine.

---
+++ src/Nethereum.eShop/ApplicationCore/Entities/RuleTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nethereum.eShop.ApplicationCore.Entities
+{
+    public class RuleTree
+    {
+        public RuleTree(RuleTreeOrigin pOrigin)
+        {
+            TreeOrigin = pOrigin;           
+        }
+
+        #region Properties
+
+        public RuleTreeOrigin TreeOrigin { get; set; }
+
+        #endregion
+
+        #region Advanced Properties
+
+        public bool SerializeToBlockchain { get; set; }
+
+        public uint? MinGasCost { get; set; }
+
+        public uint? MaxGasCost { get; set; }
+
+        private string GroveId { get; set; }
+        
+        private uint? GroveIndex { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ src/Nethereum.eShop/ApplicationCore/Interfaces/IRuleTreeService.cs
@@ -0,0 +1,12 @@
+﻿using Nethereum.eShop.ApplicationCore.Entities;
+using System.Threading.Tasks;
+
+namespace Nethereum.eShop.ApplicationCore.Interfaces
+{
+    public interface IRuleTreeService
+    {
+        Task<RuleTree> CreateRuleTreeAsync(RuleTreeOrigin origin);
+
+        Task Execute(RuleTree TargetRuleTree);
+    }
+}
