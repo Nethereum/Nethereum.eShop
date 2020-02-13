d4daabfbfa0bdeb0f0175292c1609168f87f3b39
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-02-13 18:17:17 -0500

    Added initial version of IDataSource interface and related example classes, which will all help the rules engine retrieve data during its execution.

---
+++ src/Nethereum.eShop/ApplicationCore/Entities/RulesEngine/DatabaseSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+using Nethereum.eShop.ApplicationCore.Interfaces;
+
+namespace Nethereum.eShop.ApplicationCore.Entities.RulesEngine
+{
+    public class DatabaseSource : BaseEntity, IDataSource
+    {
+        public DatabaseSource()
+        { }
+
+        public string GetMarkupID()
+        {
+            return "N";
+        }
+
+        public bool IsRemote()
+        {
+            return true;
+        }
+    }
+}
