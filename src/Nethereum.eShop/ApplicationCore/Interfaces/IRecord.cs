d77be2d88c93938087e94a799faa8326e0913af2
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-02-21 17:46:41 -0500

    Added usage of IRecord and RuleTreeRecord for the rules engine service.

---
+++ src/Nethereum.eShop/ApplicationCore/Interfaces/IRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nethereum.eShop.ApplicationCore.Interfaces
+{
+    public interface IRecord
+    {
+        void AddData(string attrName, string attrValue);
+
+        string GetData(string attrName);
+    }
+}
