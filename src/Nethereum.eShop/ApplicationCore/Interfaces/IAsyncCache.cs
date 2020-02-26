27019524b08d6ff2673898f56cfe899a536d3f23
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-02-26 18:05:31 -0500

    Created initial revision of RuleTreeCache.

--- src/Nethereum.eShop/ApplicationCore/Interfaces/IAsyncCache.cs
+++ src/Nethereum.eShop/ApplicationCore/Interfaces/IAsyncCache.cs
@@ -6,9 +6,12 @@ namespace Nethereum.eShop.ApplicationCore.Interfaces
 {
     public interface IAsyncCache<T> where T : BaseEntity
     {
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(string id);
+
         Task<IReadOnlyList<T>> ListAllAsync();
+
         Task<T> AddAsync(T entity);
+
         Task DeleteAsync(T entity);
     }
 }
---
+++ src/Nethereum.eShop/ApplicationCore/Interfaces/IRuleTreeCache.cs
@@ -0,0 +1,10 @@
+﻿using Nethereum.eShop.ApplicationCore.Entities.RulesEngine;
+using System.Threading.Tasks;
+
+namespace Nethereum.eShop.ApplicationCore.Interfaces
+{
+    public interface IRuleTreeCache : IAsyncCache<RuleTree>
+    {
+        Task<RuleTree> GetLastRuleTreeCreatedAsync();
+    }
+}
