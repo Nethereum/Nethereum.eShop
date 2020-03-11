0c53151874baa61a716f68583c76be0704f349ea
Dave Whiffin <dave.whiffin@gmail.com>
2020-03-11 17:20:34 +0000

    Removing old batch files and updating db setup docs

--- src/Web/GenerateDbCreationScripts.bat
+++
@@ -1,4 +0,0 @@
-rem Create First Migration aka InitialCreate
-dotnet build
-dotnet ef migrations script -o c:\temp\CreateCatalogDb.sql --project ..\Nethereum.eShop --context Nethereum.eShop.Infrastructure.Data.CatalogContext --no-build
-dotnet ef migrations script -o c:\temp\CreateAppIdentityContextDb.sql --project ..\Nethereum.eShop --context Nethereum.eShop.Infrastructure.Identity.AppIdentityDbContext --no-build
